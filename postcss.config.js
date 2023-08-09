1ecd23e319c8f3c517e86b79fdaa87e6a511c9b7
沛粤 杨 <1005612968@qq.com>
2023-08-09 13:42:07 +0800

    修改设计稿宽度为750，更好的适配vant的一些默认属性值

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: {
     'postcss-pxtorem': {
-      rootValue: 100, // 设计稿宽度 / 10，假设设计稿宽度为750px
+      rootValue: 75, // 设计稿宽度 / 10，假设设计稿宽度为750px
       propList: ['*'],
       selectorBlackList: [],
     },
