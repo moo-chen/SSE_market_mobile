5b8459f3bc9872265a555277621133cad994ca43
wu-shunyu <969947885@qq.com>
2023-08-12 12:01:03 +0800

    初步完成了发帖和搜索的功能

---
+++ src/views/discover/DiscoverView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+    <van-search v-model="searchinfo" show-action
+    placeholder="请输入搜索关键词" @search="onSearch">
+    <template #action>
+    <div @click="onSearch" @keydown.enter="onSearch" tabindex="0">搜索</div>
+    </template>
+    </van-search>
+    </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      searchinfo: '',
+    };
+  },
+  methods: {
+    onSearch() {
+      this.$router.push({ name: 'home', query: { searchinfo: this.searchinfo } });
+    //   this.$router.go(0);
+    },
+  },
+};
+</script>
